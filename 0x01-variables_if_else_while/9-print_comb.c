fdf8534c3bbea9eff7fdb25fe69b2f06d8ee197d
EstebanArchbold <est.archbold.edas@gmail.com>
2020-09-18 02:02:44 +0000

    add 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+for (x = '1'; x <= '9'; x++)
+{
+putchar (x);
+putchar (44);
+putchar (32);
+}		  
+{
+putchar ('\n');
+}
+return (0);
+}
