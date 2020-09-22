2915b6f3c3b3e973a6f2f53602e20295debac34f
EstebanArchbold <est.archbold.edas@gmail.com>
2020-09-22 19:30:21 +0000

    adding

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_sign - Entry point
+ * @n: entry
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else
+{
+if (n < 0)
+{
+_putchar ('-');
+return (-1);
+}
+else
+{
+if (n == 0)
+_putchar ('0');
+return (0);
+}
+}
+}
