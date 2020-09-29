e2a422f5b563251f1b38e7b1dbf8995686ae3dbf
EstebanArchbold <est.archbold.edas@gmail.com>
2020-09-29 20:12:24 +0000

    adding 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _strlen - show the lenght of a string
+ *
+ * @s: entry
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int m;
+	for (m = 0; s[m] != '\0'; m++)
+		{}
+		return m;
+}
