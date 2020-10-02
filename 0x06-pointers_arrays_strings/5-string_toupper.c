13b4b10274846b032bcd7560597c41310199d4ce
EstebanArchbold <est.archbold.edas@gmail.com>
2020-10-02 04:02:52 +0000

    adding all

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
 /**
+ * string_toupper: return a string in upper case
  *
- *
+ *@up: entry
+ *return: the value of up
  *
  */
 char *string_toupper(char *up)
 {
 	int a;
-	for (a = 0; up[a] != '\0'; a++ )
+
+	for (a = 0; up[a] != '\0'; a++)
 	{
 		if (up[a] >= 'a' && up[a] <= 'z')
 		{
