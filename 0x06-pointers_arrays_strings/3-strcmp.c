82fef60b24ca31025042516d233017d41662589a
EstebanArchbold <est.archbold.edas@gmail.com>
2020-10-02 02:03:45 +0000

    fix 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- * *_strncpy - copies string src to buffer pointed to by dest
- * @dest: string pointer
- * @src: buffer pointer
- * @n: entry point
+ * *_strcmp - copies string src to buffer pointed to by dest
+ * @s1: string pointer
+ * @s2: buffer pointer
  * Description: cpies string src to buffer
  * Return: dest pointer
  **/
 int _strcmp(char *s1, char *s2)
 {
 	int n;
+
 	for (n = 0; s1[n] == s2[n]; n++)
 		if (s1[n] == '\0')
-			return 0;
-	return s1[n]-s2[n];
+			return (0);
+	return (s1[n] - s2[n]);
 }
