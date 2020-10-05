6d2b79096a005e8da030a8a8860f5dea509e74b9
EstebanArchbold <est.archbold.edas@gmail.com>
2020-10-05 20:11:44 +0000

    fix 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *_memcpy - copy and paste
+ *@dest: destiny
+ *@src: source
+ *@n: entry point
+ *Return: results
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -10,5 +12,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	for (; n > 0; n--)
 		dest[n - 1] = src[n - 1];
 			}
-	return dest;
+	return (dest);
 }
