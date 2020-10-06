5a012bd88b36b037b2f0b1b493ebdbb44ca63580
EstebanArchbold <est.archbold.edas@gmail.com>
2020-10-06 00:03:49 +0000

    adding 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+ * _strpbkr - compare characters from one string to the other one
+ *@s: entry point
+ *@accept: point of entry
+ *Return: the value of the numbers of characters compare
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+
+	{
+
+		for (b = 0; accept[b] != '\0'; b++)
+
+		{
+
+			if (accept[b] == s[a])
+
+			{
+
+				return (&s[a]);
+
+			}
+
+		}
+
+	}
+
+	return (NULL);
+}
