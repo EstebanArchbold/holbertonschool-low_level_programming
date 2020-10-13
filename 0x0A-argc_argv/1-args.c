35d0f84376e6834cf98309261dc769d59a4c5566
EstebanArchbold <est.archbold.edas@gmail.com>
2020-10-13 00:38:52 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: argument value print what the word say
+ *@argv: argument vector
+ *Return: zero
+ *
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	argv = argv;
+	return (0);
+}
