8978b654f791b4382a821c34226ed2a6dde4d9ea
EstebanArchbold <est.archbold.edas@gmail.com>
2020-10-19 19:59:20 +0000

    fixing betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,10 @@
 #ifndef FILE_DOG
 #define FILE_DOG
 /**
- *@name: name of the dog
- *@age: how old is the dog
- *@owner :who belong the dog
+ * struct dog - dog information adquired
+ * @name: name of the dog
+ * @age: how old is the dog
+ * @owner :who belong the dog
  */
 struct dog
 {
