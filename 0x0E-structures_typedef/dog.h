e71c9de8e49e1e5fbcbdb29d799387ab9076faff
EstebanArchbold <est.archbold.edas@gmail.com>
2020-10-19 19:56:21 +0000

    take a look

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+/**
+ *@name: name of the dog
+ *@age: how old is the dog
+ *@owner :who belong the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
