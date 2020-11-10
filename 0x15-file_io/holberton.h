c4136296e60c8d3c9d3d484a1898c90ee4014142
EstebanArchbold <est.archbold.edas@gmail.com>
2020-11-10 03:58:29 +0000

    fix holber

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,7 +1,10 @@
-#ifndef HOLBERTON
-#define HOLBERTON
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+
 #endif
